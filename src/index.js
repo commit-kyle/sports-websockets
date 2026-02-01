4725a4a577364244842d7129b4ed84984410e637
Kyle Buttress <kylebuttress11@gmail.com>
2026-02-01 18:52:48 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('Happy developing ✨')
+import express from 'express';
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from Sportz server!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
